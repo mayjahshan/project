eb6edac8c4cd778af998525dce585871778e8430
Mervatk <39992888+Mervatk@users.noreply.github.com>
2018-06-09 04:48:22 +0300

    Clothes_Screen_V1

--- MayClothes/app/src/main/java/com/example/mervatkhoury/mayclothes/MainActivity.java
+++ MayClothes/app/src/main/java/com/example/mervatkhoury/mayclothes/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.mervatkhoury.mayclothes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ok = (ImageView) findViewById(R.id.ok);
+        addListenerOnButton();
+
+    }
+
+    public void addListenerOnButton() {
+
+        ok.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Intent i = new Intent(getApplicationContext(),com.example.mervatkhoury.mayclothes.ClothesScreen.class);
+                startActivity(i);
+            }
+
+        });
+
     }
 }
